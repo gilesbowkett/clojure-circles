24929bd88fb7cbf0452307eb8dc2664f2104339b
Giles Bowkett <gilesb@gmail.com>
2013-06-29 16:39:22 -0700

    name change and use (let) for an upcoming change

--- src/circles/core.clj
+++ src/circles/core.clj
@@ -6,27 +6,26 @@
 (def the-width 808)
 (def the-height 500)
 
-; to get any further, you need circles to have a concept of velocity. you might as well
-; just make these fuckers objects at this point. probably via defrecord.
-
 (defn circle-as-list []
   (list (int (rand the-width)) (int (rand the-height))))
 
 (def circle-positions
   (atom (list (circle-as-list) (circle-as-list))))
 
-(defn circle [xy]
+(defn draw-circle [circle]
   (stroke 171 163 225)
   (stroke-weight 5)
   (fill 213 209 240)
 
   (let [diam 76
-        x (first xy)
-        y (second xy)]
+        x (first circle)
+        y (second circle)]
     (ellipse x y diam diam)))
 
 (defn move-circle [xy]
-  (list (+ 1 (first xy)) (+ 1 (second xy))))
+  (let [x (first xy)
+        y (second xy)]
+    (list (+ 1 x) (+ 1 y))))
 
 (defn move-circles [circles]
   (map move-circle circles))
@@ -34,7 +33,7 @@
 (defn draw-circles []
   (background 0)
   (swap! circle-positions move-circles)
-  (doall (map circle @circle-positions)))
+  (doall (map draw-circle @circle-positions)))
 
 (defn setup []
   (smooth)
